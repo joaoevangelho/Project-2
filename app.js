aaef5aab7b66e53dc53ff8e80949de4ef88f6117
Joao Evangelho <joaoevangelho@MacBook-Pro-de-Joao-3.local>
2019-11-25 17:25:44 +0000

    new authentication route

--- app.js
+++ app.js
@@ -1,41 +1,47 @@
-'use strict';
+"use strict";
 
 const {
   join
-} = require('path');
-const express = require('express');
-const createError = require('http-errors');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const sassMiddleware = require('node-sass-middleware');
-const serveFavicon = require('serve-favicon');
+} = require("path");
+const express = require("express");
+const createError = require("http-errors");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const sassMiddleware = require("node-sass-middleware");
+const serveFavicon = require("serve-favicon");
 
 const expressSession = require("express-session");
 const connectMongo = require("connect-mongo");
 const MongoStore = connectMongo(expressSession);
-const mongoose = require('mongoose');
-const User = require('./models/user');
+const mongoose = require("mongoose");
+const User = require("./models/user");
 
-const indexRouter = require('./routes/index');
+const indexRouter = require("./routes/index");
+const authenticationRouter = require('./routes/authentication');
+
 // const usersRouter = require('./routes/user');
 
 const app = express();
 
-app.set('views', join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(logger("dev"));
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
 app.use(cookieParser());
-app.use(serveFavicon(join(__dirname, 'public/images', 'favicon.ico')));
-app.use(sassMiddleware({
-  src: join(__dirname, 'public'),
-  dest: join(__dirname, 'public'),
-  outputStyle: process.env.NODE_ENV === 'development' ? 'nested' : 'compressed',
-  sourceMap: true
-}));
+app.use(serveFavicon(join(__dirname, "public/images", "favicon.ico")));
+app.use(
+  sassMiddleware({
+    src: join(__dirname, "public"),
+    dest: join(__dirname, "public"),
+    outputStyle: process.env.NODE_ENV === "development" ? "nested" : "compressed",
+    sourceMap: true
+  })
+);
 
 app.use(
   expressSession({
@@ -76,8 +82,11 @@ app.use((req, res, next) => {
 });
 //app.use(express.static(join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/authentication', authenticationRouter);
+
+app.use("/", indexRouter);
 // app.use('/user', usersRouter);
+
 
 // Catch missing routes and forward to error handler
 app.use((req, res, next) => {
@@ -88,10 +97,10 @@ app.use((req, res, next) => {
 app.use((error, req, res, next) => {
   // Set error information, with stack only available in development
   res.locals.message = error.message;
-  res.locals.error = req.app.get('env') === 'development' ? error : {};
+  res.locals.error = req.app.get("env") === "development" ? error : {};
 
   res.status(error.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,50 @@
 window.addEventListener('load', () => {
   console.log('Ironmaker app started successfully!');
 }, false);
+
+
+function initMap() {
+  console.log('Google library was loaded');
+  const $mapContainer = document.getElementById('map');
+
+
+  if ($mapContainer) {
+    const map = new window.google.maps.Map($mapContainer, {
+      center: {
+        lat: 38.7,
+        lng: -9.1
+      },
+      zoom: 12,
+      //disableDefaultUI: true,
+      //mapTypeId: 'satellite',
+      mapTypeId: 'roadmap', // -> default
+      //mapTypeId: 'terrain',
+      //mapTypeId: 'hybrid',
+    });
+
+    const betaICoordinates = {
+      lat: 38.729,
+      lng: -9.144
+    };
+
+    const marker = new window.google.maps.Marker({
+      position: betaICoordinates,
+      map
+    });
+
+    map.addListener('click', event => {
+      console.log(event);
+      const position = {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng()
+      };
+      //console.log(position);
+      new window.google.maps.Marker({
+        position,
+        map
+      });
+
+    })
+  }
+
+};
