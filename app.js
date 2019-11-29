39be8cceec45d21f0a5e09810888e33e803da2be
Joao Evangelho <joaoevangelho@MacBook-Pro-de-Joao-3.local>
2019-11-29 10:37:56 +0000

    infowindow

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ hbs.registerPartials(join(__dirname, 'views/partials'));
 hbs.registerHelper('ifEquals', function(arg1, arg2, options) {
   return arg1 == arg2 ? options.fn(this) : options.inverse(this);
 });
+hbs.registerHelper('ifNotEquals', function(arg1, arg2, options) {
+  return arg1 == arg2 ? options.fn(this) : options.inverse(this);
+});
+
 hbs.registerHelper('select', function(value, options) {
   return options
     .fn(this)
